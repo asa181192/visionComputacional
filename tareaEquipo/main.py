a79443a631c3eaf8f9e93fcff10323d1b5597ec2
Alfredo Santiago <asa181192@users.noreply.github.com>
2015-08-27 21:54:06 -0500

    Update main.py

--- tareaEquipo/main.py
+++ tareaEquipo/main.py
@@ -5,10 +5,10 @@ import ruido
 
 image = cv2.imread('test.jpg')#accedemos a al ruta donde se encuentra la imagen
 image2 = cv2.imread('test.jpg')
-#cv2.imshow("Original",image)#Cargamos un frame con la primer imagen a mostrar 
-(h,w) =image2.shape[:2]
-ruidos = ruido.sal(image2,0.40,w,h) ; 
+(h,w) =image2.shape[:2] # se obtiene la altura y la anchura de la matriz por el metodo shape 
+ruidos = ruido.sal(image2,0.40,w,h) ;  # se envia al metodo sal la imagen con la probabilidad y sus dimensiones . 
 
-matriz = np.hstack([image,ruidos]) 
+matriz = np.hstack([image,ruidos]) # se unen las dos imagenes la original y modificada en un solo vector para mostrarse
+                                   #en una sola ventana 
 cv2.imshow("Ruido",matriz)
 cv2.waitKey(0)
