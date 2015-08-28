3bfe531b08040a50ecaaf2e841ea8e91dc10fe06
Alfredo Santiago <asa181192@users.noreply.github.com>
2015-08-27 21:51:57 -0500

    Update ruido.py

--- tareaEquipo/ruido.py
+++ tareaEquipo/ruido.py
@@ -1,13 +1,15 @@
 import random 
 
 
-def sal(pixeles, intensidad,altura,anchura):
+def sal(pixeles, intensidad,altura,anchura): # se obtiene por parametros la imagen , la intesidad que es una probabilidad 
+					     # en float de 0.1 a -1 , la altura y la anchura de la imagen para recorrer 
+					     # su matriz . 
 
 	
-	for i in range(0,altura) :
+	for i in range(0,altura) : #se recorre todos los pixeles de la imagen 
 		for j in range (0,anchura) :
-			sal = random.uniform(0.1,1.0) 
-			pimienta = random.randint(0,1) 
+			sal = random.uniform(0.1,1.0) #se genera una probabilidad de que aparesca en el pixel ruido de 0.1 - 1 
+			pimienta = random.randint(0,1) # se genera otra probabilidad de que aparesca een el pixel un negro o blanco 
 
 			if sal <=  intensidad : 
 				
@@ -17,4 +19,4 @@ def sal(pixeles, intensidad,altura,anchura):
 				 	pixeles[i,j] = (255,255,255)
 							
 
-	return pixeles
+	return pixeles # regresamos la matriz de la iamgen 
